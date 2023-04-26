f62e64717252f1bc605d4d5d61fb3e14b1259f3e
Luis Felipe López <felipelop254@gmail.com>
2023-04-25 20:44:16 -0500

    [FEATURE] add ts-node-dev & morgan

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import cors from "cors";
 
 import router from "./routes/router";
@@ -7,6 +8,9 @@ import router from "./routes/router";
 dotenv.config();
 
 const app = express();
+
+// Morgan config
+app.use(morgan("dev"));
 
 // ------------------ Config ------------------
 app.set("port", process.env.PORT ?? 3001);
